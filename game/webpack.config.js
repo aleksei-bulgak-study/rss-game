32f4e19f678e4f0f49b9fc9d132d973bc2924b76
aleksei-bulgak-study <bulgak.aleksei@yandex.ru>
2018-12-18 01:35:43 +0300

    feat: add initial framework configuration

---
+++ game/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'html-loader',
+        },
+      },
+      {
+        test: /\.jpg$|\.png$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: './src/index.html',
+        to: '../dist/index.html',
+      },
+    ]),
+  ],
+
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
